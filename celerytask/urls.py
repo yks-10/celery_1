4a91122dacc59a4bc444c86adb63dfd57cb9ff82
yks-10 <yogeshkrishnan.cse@gmail.com>
2022-03-16 15:48:02 +0530

    conclusion

--- celerytask/urls.py
+++ celerytask/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
+from task.views import GenerateRandomUserView, UsersListView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('users/', UsersListView.as_view(), name='users_list'),
+    url('generate/', GenerateRandomUserView.as_view(), name='generate')
+
 ]
--- task/views.py
+++ task/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.views.generic import ListView
+from django.views.generic.edit import FormView
+from django.shortcuts import redirect
 
-# Create your views here.
+from .form import GenerateRandomUserForm
+from .task import create_random_user_accounts
+
+# returns a list of generated user accounts
+class UsersListView(ListView):
+    template_name = 'task/user_list.html'
+    model = User
+
+# A page with the form where we can input the number of accounts to generate
+class GenerateRandomUserView(FormView):
+    template_name = 'task/generate_random_user.html'
+    form_class = GenerateRandomUserForm
+
+    def form_valid(self, form):
+        total = form.cleaned_data.get('total')
+        create_random_user_accounts.delay(total)
+        messages.success(self.request, 'We are generating your random users! Wait a moment and refresh this page.')
+        return redirect('users_list')
